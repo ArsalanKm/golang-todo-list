770a4a679f207baa8fdaff3fd8838f3818d0bea5
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-02-21 15:35:50 +0330

    create,delete functions

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -79,13 +81,60 @@ rnd.JSON(w,http.StatusOK,renderer.M{
 })
 }
 
-func createTodo(){
+func createTodo( w http.ResponseWriter,r *http.Request){
+var t todo
+if err := json.NewDecoder(r.Body).Decode(&t);err!=nil{
+rnd.JSON(w,http.StatusProcessing,err)
+return
+}
+
+if t.Title == ""{
+	rnd.JSON(w,http.StatusBadRequest,renderer.M{
+		"messaage":"title is empty",
+	})
+}
+tm :=todoModel{
+	ID: bson.NewObjectId(),
+	Title: t.Title,
+	Completed: false,
+	createdAt: time.Now(),
+}
+if err :=db.C(collectionName).Insert(&tm);err!=nil{
+	rnd.JSON(w,http.StatusProcessing,renderer.M{
+		"message" : "Failed to Insert To Do object",
+		"err" : err,
+	})
+	return
+}
+
+rnd.JSON(w,http.StatusCreated,renderer.M{
+	"message":"todo created successfully",
+	"todo_it":tm.ID.Hex(),
+})
 
 }
 func updateTodo(){
 
 }
-func deleteTodo(){
+func deleteTodo(w http.ResponseWriter,r *http.Request){
+	id:=strings.TrimSpace(chi.URLParam(r,"id"))
+	if !bson.IsObjectIdHex(id){
+		rnd.JSON(w,http.StatusProcessing,renderer.M{
+			"message":"Todo with that Id does not exists",
+		})
+		return
+}
+if err:= db.C(collectionName).RemoveId(bson.ObjectIdHex(id));err!=nil{
+
+		rnd.JSON(w,http.StatusProcessing,renderer.M{
+			"message":"Deleting Todo Error",
+			"error":err,
+		})
+		return
+}
+rnd.JSON(w,http.StatusOK,renderer.M{
+	"message":"To deleted successfully",
+})
 
 }
 
